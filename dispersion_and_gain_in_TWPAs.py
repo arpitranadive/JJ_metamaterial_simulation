256ade2eca68eec60bbe5c513168b96b5103580e
Arpit <arpit.ranadive@gmail.com>
2021-07-23 14:11:45 +0200

    version 0.1

---
+++ dispersion_and_gain_in_TWPAs.py
@@ -0,0 +1,102 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from scipy.signal import butter,filtfilt
+from scipy.optimize import fsolve
+from scipy.integrate import odeint
+from scipy import linalg
+import scipy.constants as cst
+
+
+
+
+
+
+
+
+
+
+
+class dispersion_in_JJ_metamaterial:
+
+	def __init__(self):
+
+		self.version = 0.1
+
+		self.pump_frequency = None				# in GHz
+		self.incident_pump_amplitude = None		
+		self.phi_ext = np.pi 					# external magnetic flux in the loop		
+
+		self.loss_tangent = None				# loss tangent
+		self.number_of_loops = None				# number of loops in the transmission line
+		self.size_of_one_loop = None			# size of one SNAIL loop in m
+		self.I0 = None							# critical current of large junction
+		self.I0_ratio = None					# ratio of critical currents in the SNAIL
+		self.C_g = None							# ground capacitance
+		self.C_J = None							# junction capacitance
+
+		self.gamma = None						# kerr nonlinearity
+		self.L_loop = None						# loop inductance
+
+		self.phi_0 = 2e-15
+
+
+	def I(self,phi_s):
+
+		return self.I0_ratio*self.I0*np.sin(phi_s)+self.I0*np.sin((phi_s-self.phi_ext)/3.0)
+
+
+	def calculate_loop_parameters(self):
+
+		phi_smin = fsolve(self.I, 0)
+		gamma_tilde = (1.0/6)*(self.I0_ratio*np.cos(phi_smin)+(1.0/27)*np.cos((phi_smin-self.phi_ext)/3.0))
+		alpha_tilde = self.I0_ratio*np.cos(phi_smin)+(1.0/3)*np.cos((phi_smin-self.phi_ext)/3.0)
+		self.gamma = gamma_tilde/alpha_tilde
+		self.L_loop = abs(1.0*self.phi_0/(alpha_tilde*self.I0*2*np.pi))
+
+
+	def phase_mismatch(self,w_s):
+
+		pump_frequency = self.pump_frequency
+		pump_amplitude = self.incident_pump_amplitude
+		loss_tangent = self.loss_tangent
+		gamma = self.gamma
+		L_loop = self.L_loop
+		C_g = self.C_g
+		C_J = self.C_J
+
+		w_p = pump_frequency*1e9*2*np.pi
+		w_i = 2*w_p-w_s
+		w_0 = 1.0/(np.sqrt(L_loop*C_g))
+		w_J = 1.0/(np.sqrt(L_loop*C_J))
+		k_p = w_p/(w_0*np.sqrt(1-(w_p/w_J)**2))
+		k_s = w_s/(w_0*np.sqrt(1-(w_s/w_J)**2))
+		k_i = w_i/(w_0*np.sqrt(1-(w_i/w_J)**2))
+		w_til_p = 1-(w_p/w_J)**2
+		w_til_s = 1-(w_s/w_J)**2
+		w_til_i = 1-(w_i/w_J)**2
+		k_p_loss = loss_tangent*k_p/2
+		pump_amplitude_reduced = pump_amplitude*np.exp(-k_p_loss*self.number_of_loops/2)
+		eta_p = 3*gamma*(k_p**3)*(pump_amplitude_reduced**2)/(8*w_til_p)
+		eta_s = 3*gamma*2*(k_p**2)*k_s*(pump_amplitude_reduced**2)/(8*w_til_s)
+		eta_i = 3*gamma*2*(k_p**2)*k_i*(pump_amplitude_reduced**2)/(8*w_til_i)
+		del_k_disp = 2*k_p-k_i-k_s
+		del_kerr = 2*eta_p-eta_i-eta_s
+		del_k_total = del_k_disp + del_kerr
+		return del_k_disp, del_kerr, del_k_total
+
+
+	def phase_mismatch_vs_frequency(self,freq_array):
+		
+		del_k_disp_array = np.array([])
+		del_k_kerr_array = np.array([])
+		del_k_total_array = np.array([])
+
+		for freq in freq_array:
+
+			del_k_disp, del_kerr, del_k_total = self.phase_mismatch(freq*1e9*2*np.pi)
+			del_k_disp_array = np.append(del_k_disp_array,del_k_disp)
+			del_k_kerr_array = np.append(del_k_kerr_array,del_kerr)
+			del_k_total_array = np.append(del_k_total_array,del_k_total)
+
+		return del_k_disp_array/self.size_of_one_loop, del_k_kerr_array/self.size_of_one_loop, del_k_total_array/self.size_of_one_loop
